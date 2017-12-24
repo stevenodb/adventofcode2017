9abcfe7799394b1afbd460e8e622ac44c9f3db9e
Steven Op de beeck <steven@opdebeeck.org>
2017-12-24 14:54:58 +0100

    complete day 2 — checksum and division

---
+++ _02_corruption_checksum/test_possible_division.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from _02_corruption_checksum import integer_division
+
+class TestPossible_division(TestCase):
+    def test_possible_division(self):
+        self.assertEqual(4, integer_division.possible_division([2, 3, 7, 8]))
+        self.assertEqual(4, integer_division.possible_division([8, 3, 7, 2]))
